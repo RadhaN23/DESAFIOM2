0f98f8f5bbba503b23422dc6aa8e4ed757d8d7e5
RadhaN23 <radhaneves1@gmail.com>
2023-12-08 19:45:46 -0300

    sextou

---
+++ script.js
@@ -0,0 +1,27 @@
+
+console.log ("Ano novo está chegando")
+
+let nome= "Mariah Carey"
+
+let idade = 54;
+
+let ano = 2023
+
+const anoNascimento = ano - idade;
+console.log(anoNascimento);
+
+let quartaFeira = true;
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
+let _idade = 19
+
+if ( idade >= 18) {
+   console.log ("Você poderá ver o show");
+}else { 
+console.log ("Sinto muito, vai ficar no sofá vendo da tv globo")
+}
+
+l
